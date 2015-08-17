578cf19e62d62d9d7a6b8dd182d5f261a3253ee2
invisiblek <dan.pasanen@gmail.com>
2015-08-17 21:11:56 +0200

    g4 use g3 LG camera header for now

---
+++ include/camera/CameraParametersExtra.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define CAMERA_PARAMETERS_EXTRA_C \
+const char CameraParameters::AUDIO_ZOOM_OFF[] = "audio-zoom"; \
+const char CameraParameters::AUDIO_ZOOM_ON[] = "audio-zoom"; \
+const char CameraParameters::BEAUTY_SHOT_OFF[] = "beauty-shot"; \
+const char CameraParameters::BEAUTY_SHOT_ON[] = "beauty-shot"; \
+const char CameraParameters::BURST_SHOT_OFF[] = "burst-shot"; \
+const char CameraParameters::BURST_SHOT_ON[] = "burst-shot"; \
+const char CameraParameters::KEY_AUDIO_ZOOM[] = "audio-zoom"; \
+const char CameraParameters::KEY_AUDIO_ZOOM_SUPPORTED[] = "audio-zoom-supported"; \
+const char CameraParameters::KEY_BEAUTY_SHOT[] = "beauty-shot"; \
+const char CameraParameters::KEY_BEAUTY_SHOT_SUPPORTED[] = "beauty-shot-supported"; \
+const char CameraParameters::KEY_BURST_SHOT[] = "burst-shot"; \
+const char CameraParameters::KEY_BURST_SHOT_SUPPORTED[] = "burst-shot-supported"; \
+const char CameraParameters::KEY_FOCUS_MODE_OBJECT_TRACKING[] = "object-tracking"; \
+const char CameraParameters::KEY_FOCUS_MODE_OBJECT_TRACKING_SUPPORTED[] = "object-tracking-supported"; \
+const char CameraParameters::KEY_VIDEO_WDR[] = "video-wdr"; \
+const char CameraParameters::KEY_VIDEO_WDR_SUPPORTED[] = "video-wdr-supported"; \
+const char CameraParameters::VIDEO_WDR_OFF[] = "video-wdr"; \
+const char CameraParameters::VIDEO_WDR_ON[] = "video-wdr"; \
+const char CameraParameters::OBJECT_TRACKING_ON[] = "object-tracking"; \
+const char CameraParameters::OBJECT_TRACKING_OFF[] = "object-tracking";
+
+#define CAMERA_PARAMETERS_EXTRA_H \
+    static const char AUDIO_ZOOM_OFF[]; \
+    static const char AUDIO_ZOOM_ON[]; \
+    static const char BEAUTY_SHOT_OFF[]; \
+    static const char BEAUTY_SHOT_ON[]; \
+    static const char BURST_SHOT_OFF[]; \
+    static const char BURST_SHOT_ON[]; \
+    static const char KEY_AUDIO_ZOOM[]; \
+    static const char KEY_AUDIO_ZOOM_SUPPORTED[]; \
+    static const char KEY_BEAUTY_SHOT[]; \
+    static const char KEY_BEAUTY_SHOT_SUPPORTED[]; \
+    static const char KEY_BURST_SHOT[]; \
+    static const char KEY_BURST_SHOT_SUPPORTED[]; \
+    static const char KEY_FOCUS_MODE_OBJECT_TRACKING[]; \
+    static const char KEY_FOCUS_MODE_OBJECT_TRACKING_SUPPORTED[]; \
+    static const char KEY_VIDEO_WDR[]; \
+    static const char KEY_VIDEO_WDR_SUPPORTED[]; \
+    static const char VIDEO_WDR_OFF[]; \
+    static const char VIDEO_WDR_ON[]; \
+    static const char OBJECT_TRACKING_ON[]; \
+    static const char OBJECT_TRACKING_OFF[];
