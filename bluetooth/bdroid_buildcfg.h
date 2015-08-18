fb07ff6834c52c8f96ef15bd7870fc15a063ee04
Rashed Abdel-Tawab <rashed@linux.com>
2015-08-18 11:40:04 -0400

    g4: Fix this shit

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The CyanogenMod Project
+ * Copyright 2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "LG G4"
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BTM_DEF_LOCAL_NAME "LG G4"
+#define BTA_DISABLE_DELAY 100 /* in milliseconds */
 
 #endif
