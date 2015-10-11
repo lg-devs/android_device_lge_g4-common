253d8d0d30c03679d04d0916f813b0b1cad2a50d
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-10-11 18:47:00 +0100

    g4: fix eng build
    Change-Id: I8cff43bc933c84f4b9a24fc047eaabb37c62e4d1

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -37,12 +37,12 @@ include $(BUILD_PREBUILT)
 
 # Configuration scripts
 
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.bt.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE       := init.qcom.bt.sh
+#LOCAL_MODULE_TAGS  := optional eng
+#LOCAL_MODULE_CLASS := ETC
+#LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
+#include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.coex.sh
