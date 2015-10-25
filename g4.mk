ee55012bf704a6394c2261d9d2037032cafae239
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-10-25 11:42:00 +0000

    g4: audio: move audio_platform_info into device dirs
    Change-Id: I348dc9ddff1026e92a96ca4f5d282a15877bd0b9

--- g4.mk
+++ g4.mk
@@ -101,7 +101,6 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf
