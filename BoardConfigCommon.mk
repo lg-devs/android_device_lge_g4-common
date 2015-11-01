9f7e91d9ba5cfce30d1caab033b1e42c26858a1b
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-11-01 13:55:41 +0000

    g4: add selinux policies and enforce
    Change-Id: I7b945d17f0d5aa8584864190107b077592175fba

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,9 +15,9 @@
 
 BOARD_VENDOR := lge
 
-LOCAL_PATH := device/lge/g4-common
+COMMON_PATH := device/lge/g4-common
 
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/include
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8992
@@ -46,8 +46,8 @@ TARGET_NO_BOOTLOADER := true
 TARGET_USES_C2D_COMPOSITION := true
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 androidboot.selinux=permissive
+BOARD_CUSTOM_BOOTIMG_MK := $(COMMON_PATH)/mkbootimg.mk
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
 BOARD_KERNEL_BASE := 0x00078000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x02000000
@@ -69,7 +69,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 24897388544
 TARGET_USERIMAGES_USE_EXT4 := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
@@ -155,6 +155,26 @@ TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    device.te \
+    file_contexts \
+    genfs_contexts \
+    healthd.te \
+    mediaserver.te \
+    netmgrd.te \
+    property.te \
+    property_contexts \
+    qmuxd.te \
+    rild.te \
+    sensors.te \
+    servicemanager.te \
+    time-daemon.te \
+    thermal-engine.te \
+    usb-uicc-daemon.te \
+    vold.te \
+    wpa_supplicant.te
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
@@ -173,7 +193,7 @@ BOARD_NFC_CHIPSET := pn547
 BOARD_NFC_DEVICE := "/dev/pn547"
 
 # CMHW
-BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw/
+BOARD_HARDWARE_CLASS := $(COMMON_PATH)/cmhw/
 
 # inherit from the proprietary version
 -include vendor/lge/g4-common/BoardConfigVendor.mk
